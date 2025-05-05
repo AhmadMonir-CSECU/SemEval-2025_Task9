f6c61335330c69cf94b935a38462bfeea60d6cb1
AhmadMonir-CSECU <ahmad.csecu@gmail.com>
2025-05-05 19:38:50 +0600

    Delete Templates/Semeval2025Task9/scripts directory

--- Templates/Semeval2025Task9/scripts/Run_ST1_Hazard_Cat.sh
+++
@@ -1,5 +0,0 @@
-python ../CodeST1/main.py \
---save_path ../Data/checkpoint_hazard_cat \
---num_class 10 \
---label_pos 2 \
---label_name labels_h
--- Templates/Semeval2025Task9/scripts/Run_ST2_Product.sh
+++
@@ -1,5 +0,0 @@
-python ../CodeST2/main.py \
---save_path ../Data/checkpoint_product \
---num_class 1068 \
---label_pos 3 \
---label_name labels_p
